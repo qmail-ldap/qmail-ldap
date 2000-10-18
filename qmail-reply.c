b79995fd23ed29947982a8f84530084c83e93d81
Claudio Jeker <jeker@n-r-g.com>
2000-10-18 15:23:53 +0000

    rewrite of qmail-reply first part

--- qmail-reply.c
+++ qmail-reply.c
@@ -39,6 +39,7 @@ stralloc to={0};
 stralloc from={0};
 stralloc dtline={0};
 stralloc prog={0};
+stralloc line = {0};
 
 int main()
 {
@@ -124,8 +125,6 @@ void check_header_and_get_subject(stralloc *subject)
    int match;
    int len;
    int subj_set;
-   stralloc temp = {0};
-   stralloc line = {0};
 
    subj_set = 0;
    if (seek_begin(0) == -1) temp_rewind();
@@ -135,41 +134,39 @@ void check_header_and_get_subject(stralloc *subject)
          strerr_warn3("Unable to read message: ",error_str(errno),". (LDAP-ERR #4.3.0)",0);
          break; /* something bad happend, but we ignore it :-( */
       }
-      if( !str_diffn("\n", line.s, 1) ) {
+	  if ( line.len == 0 ) /* something is wrong, bad message */
+		  break;
+
+      if ( !str_diffn("\n", line.s, 1) ) {
          if ( subj_set == 0 ) {
-            if (!stralloc_copys(subject, "Re: Your mail")) temp_nomem();
+            if (!stralloc_copys(subject, "Your mail")) temp_nomem();
          } 
          return;
       }
       case_lowerb(line.s, (len = byte_chr(line.s,line.len,':') ) );
-      if( !str_diffn("subject:", line.s, len+1) ) {
-         temp.s = line.s+len+1; /* cut away "subject:" without ' ' */
-         temp.len = line.len-len-1; /* reducing lenght: amount lenght of "subject:" */
-         if (temp.len > 1 ) { /* subject has to be more than 1 char (normaly a \n) */
-            if (!stralloc_copys(subject, "Re:")) temp_nomem();
-            if (!stralloc_cat(subject, &temp)) temp_nomem();
-            temp.s = 0; temp.len = 0;
+      if ( !str_diffn("subject:", line.s, len+1) ) {
+         if (line.len-len-1 > 1 ) { /* subject has to be more than 1 char (normaly a \n) */
+            if (!stralloc_copyb(subject, line.s+len+1, line.len-len-1)) temp_nomem();
             subj_set=1;
          }
       }
-      if( !str_diffn("mailing-list:", line.s, len+1) ) exit(0); /* don't send to mailing-lists */
-      if( !str_diffn("precedence:", line.s, len+1) ) { /* exit if bulk, junk, list */
-         temp.s = line.s+len+1;      /* cut away the header tag and the nasty \n ...  */
-         temp.len = line.len-len-2;  /* by reducing lenght (quite a nasty hack ;-) ) */
-         if ( wild_matchb("list", 4, temp.s, temp.len) ) {
+      if ( !str_diffn("mailing-list:", line.s, len+1) ) exit(0); /* don't send to mailing-lists */
+      if ( !str_diffn("precedence:", line.s, len+1) ) { /* exit if bulk, junk, list */
+		  
+         if ( wild_matchb("list", 4, line.s+len+1, line.len-len-1) ) {
             exit(0);
          }
-         if ( wild_matchb("bulk", 4, temp.s, temp.len) ) {
+         if ( wild_matchb("bulk", 4, line.s+len+1, line.len-len-1) ) {
             exit(0);
          }
-         if ( wild_matchb("junk", 4, temp.s, temp.len) ) {
+         if ( wild_matchb("junk", 4, line.s+len+1, line.len-len-1) ) {
             exit(0);
          }
       }   
    } while (match);
-   strerr_warn1("Premature end of header AARRG, giving up. (LDAP-ERR #4.5.0)");
+   strerr_warn1("Premature end of header. AARRG! This message has no body. (LDAP-WARN #4.5.0) ignored",0);
    if ( subj_set == 0 ) {
-      if (!stralloc_copys(subject, "Re: Your mail")) temp_nomem();
+      if (!stralloc_copys(subject, "Your mail")) temp_nomem();
    } 
 }
 
@@ -206,7 +203,9 @@ void send_reply(stralloc *dtl, stralloc *to, stralloc *from, stralloc *subject, 
  write(pi[1],to->s, to->len);
  write(pi[1], "\nFrom: ", 7);
  write(pi[1], from->s, from->len);
+ write(pi[1], " (via the qmail-reply program)", 30);
  write(pi[1], "\nSubject: ", 10);
+ write(pi[1], "[Auto-Reply]", 12);
  write(pi[1], subject->s, subject->len);
  write(pi[1], "\n", 1);
  write(pi[1],replytext->s,replytext->len);
